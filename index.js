dcb8d75e1ba9bf52a9daba73fb3b6af7e52a3381
kuldeep pandey <kuldeep.msvm@gmail.com>
2022-12-01 00:00:41 +0530

    setup db

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
-app.listen(8000, () => {
+
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Db connected successfully!");
+}).catch((err) => {
+    console.log(err);
+})
+
+
+app.listen(process.env.PORT || 5000, () => {
     console.log("Backend Service is Up and running");
 })
